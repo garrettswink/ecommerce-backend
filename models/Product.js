c40e623b02def9e106d22efe52935e64035b1c15
Garrett <garrettswink421@gmail.com>
2023-09-01 17:53:29 -0400

    Models and Env Creation

--- models/Product.js
+++ models/Product.js
@@ -4,19 +4,38 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+    },
+    inventory: {
+      type: DataTypes.INTEGER,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category'
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: 'product', åå
   }
 );
 
