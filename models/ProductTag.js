8dbf769cedb7f248c4639900c889d3fb008f12e0
Garrett <garrettswink421@gmail.com>
2023-09-02 14:45:40 -0400

    Comitting Changes From Yesterday

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,28 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
-
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+      product_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'product',
+          key: 'id',
+        },
+      },
+    },
   },
   {
     sequelize,
