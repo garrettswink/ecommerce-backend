9b467cfbd5ab615d3ac3fd2b984fd2d1a41af0b3
Garrett <garrettswink421@gmail.com>
2023-09-05 18:04:06 -0400

    Trouble Shooting Put Request

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -41,6 +41,7 @@ router.post('/', async (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+ router.post('/', (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -61,7 +62,7 @@ router.post('/', async (req, res) => {
       console.log(err);
       res.status(400).json(err);
     });
-
+  });
 
 // update product
 router.put('/:id', (req, res) => {
